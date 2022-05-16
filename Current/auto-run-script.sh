1d411dc861ea5222697e091d89de3f8caee36ab6
pkhaan <pkhaan@protonmail.com>
2022-05-16 20:29:26 +0300

    Renaming the directory and constants

---
+++ Current/auto-run-script.sh
@@ -0,0 +1,3 @@
+gcc p3170173-p3170044-res.c -lpthread
+./a.out 100 1000
+read -p "Press enter to continue"
--- Current/p3170173-p3170044-res.c
+++ Current/p3170173-p3170044-res.c
@@ -5,8 +5,39 @@
 #include "p3170173-p3170044-res.h"
 
 
+/*
+ * External Variables that have access to each and every thread
+*/
+int nCust; //This counts how many customers we have
+int randSeed; //seed of the generator
+int deposit; //this stores the account balance
+int exchangeID; //this stores the number of the transaction
+int availableTelephones; //this stores the number of the available telephones
+double avgWaitTime; //Average Client Waiting Time
+double avgServeTime; //Average Client Servicing Time
+int seatPlan[Nseat]; //Theater Map
+int availableSeats; //This stores the availability of the seats
+int waitTotal; // total client waiting time
+
 
 void assert_successful_mutex_action(int response_code);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Mutex Initialization
 /*
@@ -28,10 +59,49 @@ pthread_mutex_t total_time = PTHREAD_MUTEX_INITIALIZER;
 
 int no_available_phones;
 int * seats;
+int * customerID;
 
 
 
-unsigned int seed;
+
+unsigned int seed;  //seed of the random generatort 
+
+
+
+
+void * run(void * customer_id){
+    
+clock_gettime(CLOCK_REALTIME, &beginClock); //Here the timer is set
+
+    struct timespec beginClock;
+	struct timespec	inRow;
+	struct timespec endClock;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 
 
