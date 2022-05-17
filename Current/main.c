469501b7b229bf66de3a39f78ebfeac891ea3d57
pkhaan <pkhaan@protonmail.com>
2022-05-17 20:33:55 +0300

    Renaming files for ease of compilation

---
+++ Current/main.c
@@ -0,0 +1,84 @@
+//
+// Created by peter on 5/5/2022.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+#include "p3170173-p3170044-res.h"
+/*
+ * External Variables that have access to each and every thread
+*/
+int nCust; //This counts how many customers we have
+unsigned int randSeed; //seed of the generator
+int deposit; //this stores the account balance
+int exchangeID; //this stores the number of the transaction
+int availableTelephones; //this stores the number of the available telephones
+double avgWaitTime; //Average Client Waiting Time
+double avgServeTime; //Average Client Servicing Time
+int seatPlan[1]; //Theater Map
+int availableSeats; //This stores the availability of the seats
+int waitTotal; // total client waiting time
+
+
+void assert_successful_mutex_action(int response_code);
+
+int no_available_phones;
+int * seats;
+int * customerID;
+
+
+
+//Mutex Initialization
+/*
+ * Telephones
+ * Seats
+ * Account Balance
+ *Console Output->Lock(Screen)
+ * Services
+ */
+pthread_mutex_t mutex_no_available_telephones = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_seats_available = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock_screen = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t account_balance = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t time_on_hold = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t total_time = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t availableMachinesMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t terminalMutex = PTHREAD_MUTEX_INITIALIZER;
+
+
+
+
+
+void * run(void * client_id){
+    
+
+
+    struct timespec beginClock;
+	struct timespec	inRow;
+	struct timespec endClock;
+////////////////////////////////////////////////////////////////////////
+    clock_gettime(CLOCK_REALTIME, &beginClock); //Here the timer is set
+///////////////////////////////////////////////////////////////////////
+
+
+    
+	int flag = flag_ok;
+	int cid =  *(int *)client_id;
+
+
+
+
+int main(int argc, char *argv[]){
+
+
+}
+
+}
+
+
+
+
+
+
---
+++ Current/main.h
@@ -0,0 +1,80 @@
+//
+// Created by peter on 5/5/2022.
+//
+
+#ifndef OS_ASSIGNMENT_P3170173_3170044_RES_H
+#define OS_ASSIGNMENT_P3170173_3170044_RES_H
+
+#endif //OS_ASSIGNMENT_P3170173_3170044_RES_H
+
+#pragma once
+#include <stdio.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+//DECLARATION OF CONSTANT VALUES
+/*
+In here we store tha data needed for the assignment
+*/
+
+#define no_seats_per_row 10;
+#define no_telephones 3;
+#define no_zone_alpha 10;
+#define no_zone_beta 20;
+#define p_zone_alpha 0.3F;
+#define p_card_success 0.9F;
+#define cost_per_seat_zoneA 30UL;
+#define cost_per_seat_zoneB 20UL;
+#define no_seat_low 1;
+#define no_seat_high 5;
+#define time_res_high 1L;
+#define time_res_high 5L;
+#define time_seat_low 5L;
+#define time_seat_high 13L;
+#define time_cash_low 4L;
+#define time_cash_high 8L;
+
+
+
+
+
+//function that makes the call
+void *makeCall(void *threadId);
+
+
+//DECLARATION OF ASSISTING METHODS IN THE PROGRAM
+/*Prints the final message for the customer if everything was successful */
+void  print_complete_transaction(int cid, const int* seats, int seats_total)
+{
+    printf("Your reservation has been completed. \nid customer is:  %d \nand your seats are: ", cid);
+    for (int i = 0; i < no_seats; i++)
+    {
+        if (seats[i] == cid) printf("%d seat, ", i);
+    }
+    printf("\nThe total amount is  %.2f € \n\n", seats_total*cost_per_seat);
+
+}
+
+void print_fail_no_available_seats(){
+    printf("Your reservation failed due to no availability of seats.")
+}
+
+void print_fail_credit_card(){
+    printf("Your reservation failed due to credit card issues.")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
